80396cc523d400546b16ff21005cb96aea4eb512
L245T <34186595+L245T@users.noreply.github.com>
2024-06-12 01:19:03 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker-proxy.l245t.download": "https://registry-1.docker.io",
+  "quay-proxy.l245t.download": "https://quay.io",
+  "gcr-proxy.l245t.download": "https://gcr.io",
+  "k8s-gcr-proxy.l245t.download": "https://k8s.gcr.io",
+  "k8s-proxy.l245t.download": "https://registry.k8s.io",
+  "ghcr-proxy.l245t.download": "https://ghcr.io",
+  "cloudsmith-proxy.l245t.download": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
